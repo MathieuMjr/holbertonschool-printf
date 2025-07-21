4b142a16fc9d243acb3558906b48b9c6630907fe
Mathieu <mathieu.mjr@gmail.com>
2025-07-21 11:12:00 +0200

    add description

--- tests/printf.c
+++ tests/printf.c
@@ -1,5 +1,14 @@
 #include "main.h"
 
+/**
+ * _printf - must print a string of char
+ * and add data from args in it, in the good
+ * format.
+ *
+ * @format: a string of char to print
+ *
+ * Return: the number of char printed
+ */
 int _printf(const char *format, ...)
 {
 	return ();
