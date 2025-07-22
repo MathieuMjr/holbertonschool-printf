b531027d0c3fadbc800311205dbcd79b5d317de6
Mathieu <mathieu.mjr@gmail.com>
2025-07-22 16:24:49 +0200

    copyed c files for test to root directory - ready to be tested

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * struct type - associates a format specifier letter with a function
+ * to print an argument of that type.
+ *
+ * @letter: format specifier character as a string.
+ * @f: pointer to a function that prints an argument of the given type.
+ */
+typedef struct type
+{
+	char *letter;
+	void (*f)(va_list);
+} type_t;
+
+int _printf(const char *format, ...);
+int _strlen(const char *s);
+int _putchar(char c);
+void print_string(va_list args);
+void print_percent(va_list args);
+void print_char(va_list args);
+
+#endif
---
+++ printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _printf - must print a string of char
+ * and add data from args in it, in the good
+ * format.
+ *
+ * @format: a string of char to print
+ *
+ * Return: the number of char printed
+ */
+int _printf(const char *format, ...)
+{
+	type_t data[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL},
+	};
+
+	int i = 0;
+	int j = 0;
+	va_list args;
+
+	va_start(args, format);
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			j = 0;
+			while (data[j].letter != NULL)
+			{
+				if (format[i + 1] == data[j].letter[0])
+				{
+					data[j].f(args);
+					i += 2;
+					break;
+				}
+				else
+				{
+					++j;
+				}
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			++i;
+		}
+	}
+	if (i != 0)
+		return (i - 1);
+	return (0);
+}
