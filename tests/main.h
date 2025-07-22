aae95a9fac264f4e4b48159dcec2626d2e25d669
Mathieu <mathieu.mjr@gmail.com>
2025-07-22 10:55:06 +0200

    Update main with putchar function and struct type declaration

--- tests/main.h
+++ tests/main.h
@@ -4,7 +4,21 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct type - associates a format specifier letter with a function
+ * to print an argument of that type.
+ *
+ * @letter: format specifier character as a string.
+ * @f: pointer to function that prints an argument of the given type.
+ */
+typedef struct type
+{
+	char *letter;
+	void (*f) (va_list);
+} type_t;
+
 int _printf(const char *format, ...);
 int _strlen(const char *s);
+int _putchar( char c);
 
 #endif
