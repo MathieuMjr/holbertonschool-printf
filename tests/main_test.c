faacad86faf2e9154f2c3b0343c0781a1789f88c
Mathieu <mathieu.mjr@gmail.com>
2025-07-22 16:21:29 +0200

    updated test conditions

--- tests/main_test.c
+++ tests/main_test.c
@@ -3,9 +3,19 @@
 
 int main(void)
 {
-	_printf("Hello\n");
-	printf("Vérification : Hello a 5 caractères, null bytes exclu\n");
-	_printf(NULL);
-	printf(NULL);
+	_printf("Hello, %s.\n", "Vincent");
+	printf("Vérification Hello Vincent : ");
+	printf("Hello, %s.\n", "Vincent");
+	/*_printf(NULL);
+	printf("Vérification pour NULL : ");
+	printf(NULL);*/
+	_printf("Promotion de 20 %% sur les %s.\n","chemises");
+	printf("Vérification Promotion :");
+	printf("Promotion de 20 %% sur les %s\n","chemises");
+	_printf("50%% offerts pour 3 %s achetés de taille %c.\n", "pantalons", 'S');
+	printf("Vérification 50 pourcent : ");
+	printf("50%% offerts pour 3 %s achetés de taille %c.\n", "pantalons", 'S');
+	_printf("Je veux prouver que je peux utiliser plein d'arguments ! %c %s %s %c %s %c\n", 'S', "chemises", "pantalons", 'L', "jupes", 'M');
+	printf("Je veux prouver que je peux utiliser plein d'arguments ! %c %s %s %c %s %c\n", 'S', "chemises", "pantalons", 'L', "jupes", 'M');
 	return(0);
 }
