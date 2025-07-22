7257855f140e8a2b91796e6d24dd57d130b64099
Mathieu <mathieu.mjr@gmail.com>
2025-07-22 16:21:19 +0200

    main h updated with new functions from print_type.c

--- tests/main.h
+++ tests/main.h
@@ -10,16 +10,19 @@
  * to print an argument of that type.
  *
  * @letter: format specifier character as a string.
- * @f: pointer to function that prints an argument of the given type.
+ * @f: pointer to a function that prints an argument of the given type.
  */
 typedef struct type
 {
 	char *letter;
-	void (*f) (va_list);
+	void (*f)(va_list);
 } type_t;
 
 int _printf(const char *format, ...);
 int _strlen(const char *s);
-int _putchar( char c);
+int _putchar(char c);
+void print_string(va_list args);
+void print_percent(va_list args);
+void print_char(va_list args);
 
 #endif
