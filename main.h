164ff4917715f12a45faee5cc129e74d1c4f879f
Mathieu <mathieu.mjr@gmail.com>
2025-07-23 11:18:09 +0200

    Copyed modified files in root, ready to be merged and tested by checker

--- main.h
+++ main.h
@@ -21,8 +21,8 @@ typedef struct type
 int _printf(const char *format, ...);
 int _strlen(const char *s);
 int _putchar(char c);
-void print_string(va_list args, int* count);
-void print_percent(va_list args, int* count);
-void print_char(va_list args, int* count);
+void print_string(va_list args, int *count);
+void print_percent(va_list args, int *count);
+void print_char(va_list args, int *count);
 
 #endif
