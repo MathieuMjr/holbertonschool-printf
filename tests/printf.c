6d91ea4a101076ad38e8d6348cba17769522a130
Mathieu <mathieu.mjr@gmail.com>
2025-07-23 11:59:02 +0200

    Fix : try to make printf return -1 if NULL passed to it

--- tests/printf.c
+++ tests/printf.c
@@ -22,6 +22,8 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+	if (format == NULL)
+		return (-1);
 	while (format != NULL && format[i] != '\0')
 	{
 		if (format[i] == '%')
