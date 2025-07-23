14979574957fa1d5efb383373997d0927a5f53af
Mathieu <mathieu.mjr@gmail.com>
2025-07-23 12:00:10 +0200

    Fix : try to return correct value if NULL passed to function _printf

--- tests/printf.c
+++ tests/printf.c
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 	if (format == NULL)
-		return (-1);
+		return (0);
 	while (format != NULL && format[i] != '\0')
 	{
 		if (format[i] == '%')
