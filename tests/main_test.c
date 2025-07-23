eb5e3c0cf2690b691f2b221b912fbf9bd7dc45dd
Mathieu <mathieu.mjr@gmail.com>
2025-07-23 16:30:00 +0200

    new test for digit and number cases

--- tests/main_test.c
+++ tests/main_test.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Len : %d\n", len);
 	printf("Len2 : %d\n", len2);*/
-	_printf("Promotion de 20 %% sur les %s.\n","chemises");
+	/*_printf("Promotion de 20 %% sur les %s.\n","chemises");
 	printf("Vérification Promotion :");
 	printf("Promotion de 20 %% sur les %s\n","chemises");
 	_printf("50%% offerts pour 3 %s achetés de taille %c.\n", "pantalons", 'S');
@@ -24,7 +24,7 @@ int main(void)
 	_printf("Je veux prouver que je peux utiliser plein d'arguments ! %c %s %s %c %s %c\n", 'S', "chemises", "pantalons", 'L', "jupes", 'M');
 	printf("Je veux prouver que je peux utiliser plein d'arguments ! %c %s %s %c %s %c\n", 'S', "chemises", "pantalons", 'L', "jupes", 'M');
 	_printf("Hello, %c\n", 'V');
-	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);*/
 	/*len = _printf("%");
 	printf("Vérification pour % : ");
 	len2 = printf("%");
@@ -34,6 +34,7 @@ int main(void)
 	_printf("ttttttt%");
 	_printf("3%tttt");
 	_printf("Unknown:[%r]\n");*/
+	_printf("%d\n", 295);
 
 	return(0);
 }
