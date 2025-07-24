fcda10b0311c36d3adc28f94ac1a7d2bc41f31c8
Mathieu <mathieu.mjr@gmail.com>
2025-07-24 15:33:06 +0200

    Copied fonctional function is_known in tests

---
+++ tests/is_known.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int is_known (char c)
+{
+	char *list = "csid%";
+	int i = 0;
+
+	while(c != list[i] && list[i] != '\0')
+	{
+		++i;
+	}
+	printf("Valeur de i : %d\n", i);
+	if (i <= 4)
+	{
+		return(0);
+	}
+	else
+	{
+		return(-1);
+	}
+}
